1d0ef57373e1b00c1337bdb2d4fef7b2eb5d67ec
Juan Francisco Cardona McCormick <juancardona@seven4n.com>
2020-09-30 06:39:26 -0500

    Updating names instead of Main i will use App

---
+++ src/main/java/com/s4n/campus/reincarnating/App.java
@@ -0,0 +1,23 @@
+package com.s4n.campus.reincarnating;
+
+public class App {
+    
+    public static void main(String[] args) {
+	InfoProcess infoProcesses[] = { new InfoProcess("/home/juancardona/Workbench/suicidalprocess/bin", "normalprocess", 10, 10000L),
+	    new InfoProcess("/home/juancardona/Workbench/suicidalprocess/bin", "suicidalprocess", 0, 1000L)
+	};
+	ProcessCtrl processesCtrled[] = new ProcessCtrl[infoProcesses.length];
+	
+	for (int i = 0; i < infoProcesses.length; i++) {
+	    processesCtrled[i] = new ProcessCtrl(infoProcesses[i]);
+	    processesCtrled[i].start();
+	}
+
+	// This is a service will wait forever
+	try {
+	    Thread t = Thread.currentThread();
+	    t.join();
+	}
+	catch (InterruptedException ie) { }
+    }
+}
